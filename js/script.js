fd92107fe6bd5f12a858812aaf4bdb49c1848f6c
anisen <ananavelez@gmail.com>
2017-05-16 13:56:07 -0400

    ejercicio guiado son el extra

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 function agregar(){
-	
+	//recuperamos texto ingresado en la tarea y limpio textarea.
+	var tareas=document.getElementById('tarea').value;
+	document.getElementById('tarea').value="";
+    //donde estaran las tareas que recupere
+	var cont =document.getElementById("contenedor");
+    //creamos nodos de elementos
+    var nuevaTarea=document.createElement("div");
+    var textoNuevaTarea=document.createTextNode(tareas);
+    var elementoContenedor=document.createElement("span");
+    //asignamos padres a nodos creados
+    elementoContenedor.appendChild(textoNuevaTarea);
+    nuevaTarea.appendChild(elementoContenedor);
+    cont.appendChild(nuevaTarea);
+    //creamos checkbox e iconos, atributos y clases
+    var chck=document.createElement('input');
+    chck.type='checkbox';
+    chck.setAttribute('class','check');
+    var basura=document.createElement('span');
+    basura.classList.add('fa', 'fa-trash-o');
+    var cora=document.createElement('span');
+    cora.classList.add('fa', 'fa-heart');
+    //asignar padres a nodos
+    nuevaTarea.appendChild(chck);
+    nuevaTarea.appendChild(basura);
+    nuevaTarea.appendChild(cora);
+    //agregamos evento al hacer click para eliminar o agregar la clase "tachado" a elementoContenedor
+    chck.addEventListener('click', function (){
+    	elementoContenedor.classList.toggle('tachado');
+    });
+    //remover el icono basurero al hacer click
+    basura.addEventListener('click', function(){
+    	cont.removeChild(nuevaTarea);
+    });
+    //funcion click para el corazon
+    cora.addEventListener('click', function(){
+    	cora.classList.toggle('red');
+    })
 }
 
 
